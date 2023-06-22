371e6b74318410bc1a3355fd2c1b586f8dbc5dde
root <linhn0478@gmail.com>
2023-06-22 12:37:42 +0000

    s

--- client/src/components/videoDetail/DescriptionDisplay.jsx
+++ client/src/components/videoDetail/DescriptionDisplay.jsx
@@ -2,6 +2,19 @@ import { useState, useEffect } from "react"
 // "rgba(62, 166, 255, 1)" "#065fd4"
 const DescriptionDisplay = ({text=''}) => {
     const [componentArr, setComponentArr] = useState([])
+    const [hasMore, setHasMore] = useState(false)
+    const [showingMore, setShowingMore] = useState(false)
+    const [hovering, setHovering] = useState(false)
+    const [mouseDown, setMouseDown] = useState(false)
+
+    const mouseEntered = () => { setHovering(true) }
+    const mouseLeft = () => { setHovering(false) }
+    const clickedButton = () => {
+        setShowingMore(true)
+    }
+    const mouseDowned = () => { setMouseDown(true) }
+    const mouseUpped = () => { setMouseDown(false) }
+
     useEffect(()=>{
         var textArr = text.split('\n')
         const res = textArr.map(o=>{
@@ -19,12 +32,26 @@ const DescriptionDisplay = ({text=''}) => {
             return <p>{[...arr]}</p>
         })
         setComponentArr(res)
+        setShowingMore(false)
+        if (res.length > 4) {setHasMore(true)}
+        else {setHasMore(false)}
         console.log("textDisplay", res)
     },[text])
 
+    useEffect(()=>{
+
+    }, [hasMore, showingMore])
     return (
-        <div style={{color:"white", fontSize:"14px", backgroundColor:"rgba(233, 233, 233, 0.3)", padding:"20px"}}>
-            {componentArr}
+        <div className={hasMore && !showingMore ? "hover-pointer" : "none"}
+        style={{color:"white", fontSize:"14px",  padding:"20px" ,borderRadius:"15px"
+            ,backgroundColor:hasMore && !showingMore && hovering ?
+            mouseDown ? "rgba(255,255,255, 0.5)" : "rgba(255,255,255, 0.4)"
+            : "rgba(255,255,255, 0.3)"}}
+        onMouseEnter={mouseEntered} onMouseLeave={mouseLeft} onClick={clickedButton}
+            onMouseDown={mouseDowned} onMouseUp={mouseUpped}>
+            { (hasMore && !showingMore) ? componentArr.slice(0,5) : componentArr} 
+
+            {hasMore && !showingMore ? <p>...Show more</p> : null}
         </div>
     )
 }
