72c331d46d1f8418476e3858fdeb18e0ae6b0eb9
Linh Nguyen <linhn0478@gmail.com>
2023-06-30 10:55:16 +0700

    fuck

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+# docker compose -f /var/www/fullstack-youtube/docker-compose.prod.yaml up --build
 docker compose -f ./docker-compose.prod.yaml up --build
+
 
 # docker-compose -f docker-compose.yaml up --build
