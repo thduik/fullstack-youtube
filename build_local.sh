abc0d9bb7252a2b8b1916a20a14162292b173f4f
Linh Nguyen <linhn0478@gmail.com>
2023-07-01 10:34:30 +0700

    small stuff

--- build_local.sh
+++
@@ -1,4 +0,0 @@
-#testing prod build to make sure no bug in prod build process
-docker compose -f ./docker-compose.prod.yaml up --build --remove-orphans 
-
-docker compose -f ./docker-compose.yaml up --build --remove-orphans
--- z_build_scripts/build_prod.sh
+++ z_build_scripts/build_prod.sh
@@ -1,8 +1,8 @@
-# docker compose -f ./docker-compose.prod.yaml up --build --wait
+# docker compose -f ./docker-compose.prod.yaml up --build --wait --remove-orphans
 # docker compose -f ./docker-compose.prod.yaml start 
 docker compose up
-docker compose -f /var/www/fullstack-youtube/docker-compose.prod.yaml up --build
-docker compose -f /var/www/fullstack-youtube/docker-compose.prod.yaml start --build
+docker compose -f /var/www/fullstack-youtube/docker-compose.prod.yaml up --build --wait --remove-orphans
+# docker compose -f /var/www/fullstack-youtube/docker-compose.prod.yaml start --build
 
 
 # docker-compose -f docker-compose.yaml up --build
