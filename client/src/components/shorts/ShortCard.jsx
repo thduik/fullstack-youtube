973148c3b45bcb5817afec4701cd2b8b2c19ed01
Linh Nguyen <linhn0478@gmail.com>
2023-07-04 00:01:22 +0700

    fixing shorts and short card etc

--- client/src/components/shorts/ShortCard.jsx
+++ client/src/components/shorts/ShortCard.jsx
@@ -1,13 +1,20 @@
+import { useEffect, useState } from "react";
 import { normalFontColor } from "../../configs";
 
 
 const ShortCard = ({snippet: {stats:{viewsText=null}, thumbnails=[], title=null,videoId=null}, channelId=null}) => {
-    
+    const [titleDisplay, setTitleDisplay] = useState("")
+    useEffect(()=>{
+        if (!title) {return}
+        var displayTitle = title.substring(0,50)
+        if (title.length > 50) {displayTitle += "..."}
+        setTitleDisplay(displayTitle)
+    },[titleDisplay])
     return (
         <div style={{height:"500px", width:"230px", margin:"10px 20px 20px 20px"}}>
             <img height="400px" width="230px"style={{borderRadius:"20px"}} src={thumbnails[0] ? thumbnails[0].url : ""} />
-            <div>
-                <p style={{color:normalFontColor, fontSize:"13px", fontWeight:"bold"}}>{title}</p>
+            <div style={{padding:"10px 5px 8px 5px"}}>
+                <p style={{color:normalFontColor, fontSize:"14px", fontWeight:"bold", lineHeight:"1.4"}}>{titleDisplay}</p>
 
             </div>
         </div>
