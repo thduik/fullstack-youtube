96c17ae6a4648ab329e36fe7b9926860fa2e3e63
Linh Nguyen <linhn0478@gmail.com>
2023-07-10 14:30:31 +0700

    fixed bugs

--- client/src/components/shorts/ShortsPlayer.jsx
+++ client/src/components/shorts/ShortsPlayer.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import VideoPlayer from "../VideoPlayer";
 
 const ShortsPlayer = ({initialVideoId = null //curr videoId when mounted
 }) => {
     const {shortsArr} = useSelector((state)=>state.shorts)
-    const {videoId, setVideoId} = useState(null)
+    const [videoId, setVideoId] = useState(null)
+    const [currIdx, setCurrIdx] = useState(0)
     useEffect(()=>{
+        console.log("initialVideoId",initialVideoId)
         setVideoId(initialVideoId)
-    },[])
+        setCurrIdx(0)
+    },[initialVideoId])
+    useEffect(()=>{
+        console.log("shortsArr useEfect", shortsArr)
+    },[shortsArr])
     return (
         videoId ?
-        <div>
+        <div style={{ width: "100%", height: "100%", overflowY: 'hidden'}}>
                 <VideoPlayer controls={0} videoId={videoId} loop={1} />
 
         </div>
